2622157d95a55d70e159fab08c114740456febb7
lcf <lichengfeng.1996@qq.com>
2018-09-04 14:51:58 +0800

    ssm框架整合

---
+++ qingke-manager/qingke-manager-web/src/main/java/com/qingke/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.qingke.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public void index() {};
+}
